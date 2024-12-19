ecf61b7bdbe0d2d327e350c4af89649218eda148
ajmainwasif01 <nipunazmun@gmail.com>
2024-12-19 17:45:22 +0600

    Sets and Arrays

---
+++ Arrays.py
@@ -0,0 +1,10 @@
+import array as arr
+
+n = arr.array('i',[1,2,3,1,9,7,8,4,5])
+
+#print(n)
+
+print(n.count(1))
+
+n.reverse()
+print(n)
---
+++ Set_Intersection.py
@@ -0,0 +1,18 @@
+a = {2,6,4,3,1}
+b = {5,7,2,4,1}
+
+c = a | b
+#print(c)
+
+d = a.union(b)
+#print(d)
+
+e = a & b
+
+#print(e)
+
+f = a.intersection(b) 
+#print(f)
+
+u = {1,2,3,4,5,6,7,8}  
+print(u - a)
